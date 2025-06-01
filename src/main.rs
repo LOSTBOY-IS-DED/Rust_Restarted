5c3dad894bb50e9fecad5c61fd1d4b27cb3af851
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2025-06-01 23:17:27 +0530

    learnt about bools

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,21 @@ fn main() {
     println!("Hi there ishika mam");
     let ans: u32 = sum(10, 20);
     println!("Ans is {}", ans);
+    print!("Is 10 even? {}", is_even(10));
 }
 
 fn sum(a: u32, b: u32) -> u32 {
     return a + b;
 }
+
+fn is_even(num: u32) -> bool {
+    return num % 2 == 0;
+}
+
+// signed -> both postive and negative
+// // unsigned -> only positive
+// u32
+// usize
+// u64
+// u128
+// u8
