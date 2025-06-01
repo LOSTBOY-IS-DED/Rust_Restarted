b63bbccedd18eab06bcd4225e36414d43fcbe420
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2025-06-01 23:56:16 +0530

    easy bits learnt

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,14 @@ fn main() {
     let ans: u32 = sum(10, 20);
     println!("Ans is {}", ans);
     print!("Is 10 even? {}\n", is_even(10));
-    string::say_hello("ishika");
+
+    // string     string::say_hello("ishika");
     string::name();
+
+    //array
     array::array();
+
+    //vectors
     vectors::vectors();
 }
 
