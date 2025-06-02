dc25d7ed0e344c7ff4f7604ec71848cedeb1e853
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2025-06-02 21:55:33 +0530

    fix 1 transferring back the ownership

--- part-2/src/main.rs
+++ part-2/src/main.rs
@@ -1,9 +1,21 @@
+// fn main() {
+//     let str: String = String::from("Hello Ishika");
+
+//     println!("{}\n", get_length(str));
+//     println!("{}\n", str);
+// }
+// fn get_length(str: String) -> usize {
+//     return str.len();
+// }
+
+// Fix transferring back the ownership
+
 fn main() {
     let str: String = String::from("Hello Ishika");
-
-    println!("{}\n", get_length(str));
+    let (len, str) = get_length(str);
+    println!("Length is {} \n", len);
     println!("{}\n", str);
 }
-fn get_length(str: String) -> usize {
-    return str.len();
+fn get_length(str: String) -> (usize, String) {
+    return (str.len(), str);
 }
