d671df6c348f8e2f6ed359dd759d3ed7094e08e6
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2025-06-02 22:02:41 +0530

    fix 3 references

--- part-2/src/main.rs
+++ part-2/src/main.rs
@@ -10,12 +10,25 @@
 
 // Fix transferring back the ownership
 
+// fn main() {
+//     let str: String = String::from("Hello Ishika");
+//     let (len, str) = get_length(str);
+//     println!("Length is {} \n", len);
+//     println!("{}\n", str);
+// }
+// fn get_length(str: String) -> (usize, String) {
+//     return (str.len(), str);
+// }
+
+// Fix the ownership of the string --> borrowing the variable
+
 fn main() {
-    let str: String = String::from("Hello Ishika");
-    let (len, str) = get_length(str);
+    let str = String::from("Hello Ishika");
+    let len = get_length(&str);
     println!("Length is {} \n", len);
     println!("{}\n", str);
 }
-fn get_length(str: String) -> (usize, String) {
-    return (str.len(), str);
+
+fn get_length(str: &String) -> usize {
+    return str.len();
 }
