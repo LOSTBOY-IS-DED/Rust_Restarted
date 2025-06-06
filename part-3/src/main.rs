911c4451440873240b9b321cf0b1a2ce6127b9b4
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2025-06-06 19:09:45 +0530

    decalarative macro vs procedural macro

--- part-3/src/main.rs
+++ part-3/src/main.rs
@@ -32,49 +32,80 @@ mod display;
 
 // what are traits ? similar as interfaces in ts we can implement them
 
-trait Shape {
-    fn area(&self) -> u32;
-}
+// trait Shape {
+//     fn area(&self) -> u32;
+// }
 
-struct Rect {
-    width: u32,
-    height: u32,
-}
+// struct Rect {
+//     width: u32,
+//     height: u32,
+// }
 
-impl Shape for Rect {
-    fn area(&self) -> u32 {
-        return self.width * self.height;
-    }
-}
+// impl Shape for Rect {
+//     fn area(&self) -> u32 {
+//         return self.width * self.height;
+//     }
+// }
 
-struct Circle {
-    radius: u32,
-}
+// struct Circle {
+//     radius: u32,
+// }
 
-impl Shape for Circle {
-    fn area(&self) -> u32 {
-        return self.radius * self.radius;
-    }
-}
+// impl Shape for Circle {
+//     fn area(&self) -> u32 {
+//         return self.radius * self.radius;
+//     }
+// }
 
-fn main() {
-    let r = Rect {
-        width: 10,
-        height: 10,
-    };
-    let circle = Circle { radius: 20 };
-    let circle_ans = get_area(circle);
-    let ans = get_area(r);
-    println!("{}", circle_ans);
-    println!("{}", ans)
-}
+// fn main() {
+//     let r = Rect {
+//         width: 10,
+//         height: 10,
+//     };
+//     let circle = Circle { radius: 20 };
+//     let circle_ans = get_area(circle);
+//     let ans = get_area(r);
+//     println!("{}", circle_ans);
+//     println!("{}", ans)
+// }
 
-fn get_area(s: impl Shape) -> u32 {
-    return s.area();
-}
+// fn get_area(s: impl Shape) -> u32 {
+//     return s.area();
+// }
 
 // can you implement this for circle in the next commit  ?
 // step 1 : create struct
 // step 2 : implement struct
 // step 3 : create variable
 // step 4 : call the function
+
+// macros-> declarative macro , procedural micro
+
+// there is a struct i haven't implemented it yet
+
+use std::{fmt::format, fmt::Display};
+
+#[derive(Debug)]
+struct User {
+    username: String,
+    password: String,
+    age: u32,
+}
+
+// impl Debug for User {
+// derive line just added this piece of code
+// }
+
+fn main() {
+    println!("Hello world !!!");
+
+    let user = User {
+        username: String::from("Subh"),
+        password: String::from("password"),
+        age: 32,
+    };
+
+    println!("{:?}", user); // Debug print
+                            // println!("{}", user); // Pretty Display print
+    println!("Users username is {}", user.username)
+}
